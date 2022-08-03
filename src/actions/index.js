1aecf9191c7575cbfce68d7b2a542347d0989693
sadaab2784 <ali.sadaab825313@gmail.com>
2022-08-03 23:39:56 +0530

    Removing movies from favourites

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,7 +11,9 @@
 
 // this variables are called as action type
 export const ADD_MOVIES = 'ADD_MOVIES';
-export const ADD_FAVOURITE = 'ADD_FAVOURITE';
+export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES';
+export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES';
+
 
 // this function are called action creators for add movies
 export  function addMovies (movies) {
@@ -25,9 +27,16 @@ export  function addMovies (movies) {
 // this function are called action creators for add favourite
 export  function addFavourite (movie) {
   return {
-      type: ADD_FAVOURITE,
+      type: ADD_TO_FAVOURITES,
       // movies: movies
       movie
     }
+}
+
+export function removeFromFavourites(movie) {
+  return {
+    type: REMOVE_FROM_FAVOURITES,
+    movie
+  };
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_MOVIES, ADD_FAVOURITE } from '../actions';
+import { ADD_MOVIES, ADD_TO_FAVOURITES, REMOVE_FROM_FAVOURITES } from '../actions';
 
 // change the current state to favoutite state 
 const initialMoviesState = {
@@ -24,11 +24,21 @@ export default function movies (state = initialMoviesState, action) {
                 list: action.movies
             }
         
-        case ADD_FAVOURITE:
+        case ADD_TO_FAVOURITES:
             return {
                 ...state,
                 favourites: [action.movie, ...state.favourites]
             }
+        
+        case REMOVE_FROM_FAVOURITES:
+            const filteredArray = state.favourites.filter(
+                movie => movie.Title !== action.movie.Title
+            );
+
+            return {
+                ...state,
+                favourites: filteredArray
+            };
         default:
             return state;
         }
